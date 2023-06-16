efb29eb86e2cef3a5f2ac66a18412a579bcacb6c
Junhyuk Kim <121940381+kimchoker@users.noreply.github.com>
2023-06-16 20:38:36 +0900

    저장

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,64 @@ import React from "react";
 import { styled } from 'styled-components';
 import HeaderBar from "../components/HeaderBarNavi";
 import KakaoMap from "../components/KakaoMap";
-
+import { SlMenu } from "react-icons/sl";
+import { Link } from "react-router-dom";
 const HomeDiv = styled.div`
-    width: auto;
-    height: auto;
+    width: 100vw;
+    height: 100vh;
+    
+
+    .SidebarButton {
+        width: 35px;
+        height: 35px;
+        margin-top: 200px;
+        margin-left: 200px;
+        border: transparent;
+        background-color: transparent;
+        z-index: 999;
+    }
+    .SidebarButton:hover {
+        cursor: pointer;
+    }
 `;
 
+const MapWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    z-index: 0;
+    
+`;
+
+
+const SidebarIcon = styled(SlMenu)`
+  width: 30px;
+  height: 30px;
+  margin-left: -5px;
+`;
+
+const SideBar = styled.div`
+    width: 20vw;
+    height: 100%;
+    background-color: black;
+`;
+
+
+
 const Home = () =>{
+
+    const sidebarOpen = () => {
+        
+    }
+
+
     return(
         <HomeDiv>        
             <HeaderBar/>
-            <KakaoMap/>
+                <button className="SidebarButton" onClick={sidebarOpen}><SidebarIcon/></button>
+            <MapWrapper>
+                <KakaoMap/>
+            </MapWrapper>
+            
         </HomeDiv>
     );
 };
