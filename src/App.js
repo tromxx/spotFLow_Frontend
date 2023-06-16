1b31dc9a9f0a69a678a897e64ab68d97ea18b828
sk <sungkeno3o@gmail.com>
2023-06-16 20:40:53 +0900

    타임라인 작업중

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Diary from './pages/Diary';
+import TimeLine from './pages/TimeLine';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/diary" element={<Diary/>} />      
+        <Route path="/diary" element={<Diary/>} /> 
+        <Route path="/timeline" element={<TimeLine/>} />      
       </Routes>
     </BrowserRouter>
   );
