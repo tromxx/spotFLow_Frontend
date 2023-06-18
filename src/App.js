4437d04a049e86961c8c498a4e3f8344b038d199
husik927@gmail.com <husik927@gmail.com>
2023-06-18 21:55:35 +0900

    커밋 오류 수정 app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Diary from './pages/Diary';
+import MyFlow from './pages/MyFlow';
+import TimeLine from './pages/TimeLine';
+import HeaderBar from './components/HeaderBarNavi';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/diary" element={<Diary/>} />      
+        <Route path="/" element={<>
+            <HeaderBar />
+            <Home />
+          </>} />
+        <Route path="/diary" element={<Diary/>} />
+        <Route path="/myflow" element={<MyFlow/>}/>   
+        <Route path="/diary" element={<Diary/>} /> 
+        <Route path="/timeline" element={<TimeLine/>} />      
       </Routes>
     </BrowserRouter>
   );
