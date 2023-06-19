84d65b428b81b07923d95a118fd4a4f0bc350659
nanowl <hanjy20129@gmail.com>
2023-06-19 20:54:50 +0900

    메인페이지 toSpot버튼 좌표 이동 끝

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -15,16 +15,20 @@ const Container = styled.div`
 `;
 const KakaoMap=({latitude, longitude})=>{
   const {location, setLocation} = useState("");
+  let lat = 37.4923615;
+  let lng = 127.0292881;
 
 
   useEffect(()=>{
+    if (latitude > 0) lat = latitude;
+    if (longitude > 0) lng = longitude;
     let container = document.getElementById('map');
     let options = {
-      center: new kakao.maps.LatLng(37.4923615,127.0292881),
+      center: new kakao.maps.LatLng(lat, lng),
       level: 3
     };
     let map = new kakao.maps.Map(container, options);
-    }, [])
+    }, [latitude, longitude])
 
     return (
       <Container>
--- src/dataSet/ToSpotData.js
+++ src/dataSet/ToSpotData.js
@@ -3,28 +3,28 @@ const ToSpot = {
     const place = [{
       num : 1,
       location: "가로수길",
-      lat: 127.0118644,
-      lng: 37.5544853
+      lat: 37.5212557526595,
+      lng: 127.023032708155
     }, {
       num : 2,
-      location: "홍대",
-      lat: 0,
-      lng: 0
+      location: "홍대 입구",
+      lat: 37.555629126086,
+      lng: 126.92370965927591
     }, {
       num : 3,
       location: "이태원",
-      lat: 0,
-      lng: 0
+      lat: 37.54040410897566,
+      lng: 126.99216997726722
     }, {
       num : 4,
-      location: "연남동",
-      lat: 0,
-      lng: 0
+      location: "명동거리",
+      lat: 37.56576644555232,
+      lng: 126.98450443026759
     }, {
       num : 5,
-      location: "잠실",
-      lat: 0,
-      lng: 0
+      location: "롯데월드",
+      lat: 37.51116222894772,
+      lng: 127.09800673535563
     }];
     return place;
   }
