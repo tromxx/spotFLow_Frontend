2fd4343e37553a9cb69c0e7556ab996768f5fa28
Junhyuk Kim <121940381+kimchoker@users.noreply.github.com>
2023-06-21 17:51:24 +0900

    테마 수정

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,13 +3,13 @@
 export const lightTheme = {
     bgColor: 'white',
     textColor: 'black',
-    borderColor: '1px solid #d9d9d9'
+    borderColor: '1px solid #d9d9d9',
 };
 
 export const darkTheme = {
     bgColor: '#424242',
     textColor: 'white',
-    borderColor: '1px solid #424242'
+    borderColor: '1px solid #424242',
 };
 
 export const theme = {
