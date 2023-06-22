a1723367ff9d003c14c5542d924227020ccd25dd
김종연 <whddus426@gamil.com>
2023-06-22 16:52:25 +0900

    다이어리 header 부분 이미지 아이콘 searchbar 구현

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -2,6 +2,10 @@ import React from "react";
 import HeaderBar from "../components/HeaderBarNavi";
 import { styled } from 'styled-components';
 import DiaryLayout from "../components/DiaryLayout";
+import SearchBar from "../components/SearchBar";
+import avatar from "../images/default_avatar.png"
+import { BsPeople } from "react-icons/bs";
+
 
 
 const Container = styled.div`
@@ -12,9 +16,49 @@ const Container = styled.div`
     background-color: gray;
 
     .namebar{
+        display: flex;
         width: 80vw;
         height: 18vh;
+        border: solid 5px red;
+        justify-content: center;
+        align-items: center;
+        justify-content: space-between;
+        
+    .menu{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: 50px;
+        }
+        .id{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
     }
+    img{
+        width: 70px;
+        height: 70px;
+        margin-right: 20px;
+        
+    }
+    .searchBar{
+        width: 100px;
+        height: 100px;
+    }
+    .people{
+        width: 30px;
+        height: 30px;
+        display: flex;
+        justify-content: flex-end;
+        margin-right: 30px;
+    }
+    h3{
+        font-style: 
+    }
+
+    
 
 `; 
 
@@ -34,6 +78,19 @@ const Diary = () =>{
         <Container>
             <header>
                 <div className="namebar">
+                    <div className="id">
+                <img src={avatar} alt="" />
+                <h6>whddus426</h6>
+                </div>
+                <div className="searchBar1">
+                    <SearchBar  />
+                    </div>
+                            <div className="menu">
+                        <BsPeople className="people"/>
+                        <div className="MY">
+                        <h3>MY</h3>
+                        </div>
+                        </div>
                 </div>
             </header>
             <body>
