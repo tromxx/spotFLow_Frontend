b724570e9869bd18afe1467f508019ce15a68127
nanowl <hanjy20129@gmail.com>
2023-06-29 20:14:16 +0900

    diary 상세페이지 제작 중

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Modal from "../utils/modal";
+import Modal from "../utils/Modal";
 import styled from "styled-components";
 import AxiosApi from "../api/AxiosApi"
 import Policy from "../dataSet/SpotFlowPolicy";
