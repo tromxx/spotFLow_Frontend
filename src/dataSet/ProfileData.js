5b65ca516d019120fb7a7fcdf3858907b64007b7
husik927@gmail.com <husik927@gmail.com>
2023-07-04 20:32:36 +0900

    Mypage 수정

---
+++ src/dataSet/ProfileData.js
@@ -0,0 +1,10 @@
+const ProfileData = [{
+    "src" : "https://firebasestorage.googleapis.com/v0/b/spotflow-5475a.appspot.com/o/dog2.jpg?alt=media&token=b5d6a337-92a5-4881-b4c8-03b2628c911b",
+    "nickName" : "trom",
+    "following" : 450,
+    "follower" : 420,
+    "statusMsg" : "trying to test"
+}]
+
+export default ProfileData;
+    
