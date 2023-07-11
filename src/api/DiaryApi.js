d97e90ac6c0dff7724da9449adbcfed7e531e7b4
김종연 <whddus426@gamil.com>
2023-07-11 21:27:14 +0900

    다이어리api수정

--- src/api/DiaryApi.js
+++ src/api/DiaryApi.js
@@ -8,9 +8,35 @@ const DiaryApi = {
       title : title,
       content : content,
       email : email,
-      timeLineList : timeLineList
+      timeLineList : [
+        {
+          id : 1
+        }
+      ]
     }
     return axios.post(DOMAIN + "/diary", requestData);
+  },
+  findDiary : async () => {
+    const params = {
+      email: "whddus426@gmail.com"
+    };
+    return axios.get(DOMAIN + "/diary/all" , { params});
+  },
+  deleteDiary : async() => {
+    const data = {
+      "id" : 1, // 다이어리 식별번호
+      "title":"삭제된 게시글입니다.", // 제목 
+      "content" : "삭제된 게시글입니다.", // 
+      "timeLineList" : [
+          {
+              "id" : 2
+          },
+          {
+              "id" : 3
+          }
+      ]
+  }
+  return new axios.delete(DOMAIN + "/diary", data);
   }
 }
 
