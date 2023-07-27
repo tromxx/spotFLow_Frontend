14719252184017b43de1a0fbdafae92ff4da2bb3
sk <sungkeno3o@gmail.com>
2023-07-27 20:57:29 +0900

    서울축제 지도표시 기능

--- src/pages/MapView.js
+++ src/pages/MapView.js
@@ -6,6 +6,38 @@ import ToSpotData from "../dataSet/ToSpotData";
 import * as ToSpot from "../components/ToSpotComponent";
 import { LuCircleDot } from "react-icons/lu";
 import MyFlowApi from "../api/MyFlowApi";
+import ForumData from '../utils/ForumData';
+import styled from 'styled-components';
+
+const Maker = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items:center;
+  flex-direction: column;
+  font-size:10px;
+  width: 200px;
+  height: 300px;
+  background-color:white;
+  border-radius: 15px;
+
+    .img{
+      height: 70%;
+      width: 100%;
+      border-radius: 15px 15px 0 0;
+    }
+    .title{
+      display:flex;
+      justify-content: center;
+      align-items:center;
+      height: 30%;
+      width: 100%;
+      font-size: 10px;
+      flex-wrap: wrap;
+      text-overflow: ellipsis;
+      font-weight: bold;
+    }  
+`
+
 
 const MapView = React.memo((props) => {
   const navigate = useNavigate();
@@ -13,6 +45,35 @@ const MapView = React.memo((props) => {
   const [lat, setLat] = useState(37.4923615);
   const [lng, setLng] = useState(127.0292881);
   const [flow, setFlow] = useState([]);
+  const [forumData,setForumData] = useState([]);
+
+   // 축제 데이터 가져오기 
+   useEffect(() => {
+    const start_idx = 1;
+    const end_idx = 220;
+    const type = " ";
+    const title = " ";
+
+    ForumData(start_idx, end_idx, type, title).then(data => {
+      setForumData(data);
+      console.log(data);
+    }).catch(error => {
+      console.error("Error fetching forum data:", error);
+    });
+  }, []);
+
+
+  // 날짜 차이 계산
+  const calDate = (start_date) => {
+    const start_date_obj = new Date(start_date);
+    const current_date_obj = new Date();
+    const difference_in_time =  start_date_obj - current_date_obj ;
+    const difference_in_days = difference_in_time / (1000 * 3600 * 24);
+    return Math.floor(difference_in_days);
+}
+
+
+
 
   const dataInit = async (token) => {
     let res = await MyFlowApi.allFlow(token);
@@ -71,29 +132,54 @@ const MapView = React.memo((props) => {
     const map = useMap();
     const [isVisible, setIsVisible] = useState(false);
 
+    const markerImg = "https://firebasestorage.googleapis.com/v0/b/spotflow-5475a.appspot.com/o/images%2Ffree-icon-location-pin-8637632%20(1).png?alt=media&token=92a76f40-a6c7-45c9-809f-5d320b9d844f";
+    const soonImg = "https://firebasestorage.googleapis.com/v0/b/spotflow-5475a.appspot.com/o/images%2Ffree-icon-location-10797038.png?alt=media&token=e0965f50-abc4-40b0-a7eb-684052328586";
     return (
       <>
+        <div style={{display:"flex",alignItems:"center",justifyContent:"center", color:"white" ,borderRadius:"15px",backgroundColor:"#35B1E8",width:"100px",height:"30px",left:"150px",top:"60px",position:"absolute", zIndex:"100"}}>서울 축제</div>
         <MapMarker
           position={{
             lat: lat,
             lng: lng
           }}
+          image={{
+            src: calDate(content.STRTDATE) < 3 ?   soonImg : markerImg, // 마커이미지의 주소입니다
+            size: {
+              width: 45,
+              height: 50,
+            }, // 마커이미지의 크기입니다
+            options: {
+              offset: {
+                x: 27,
+                y: 69,
+              }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+            },
+          }}
+          title={content.TITLE}
           onClick={(marker) => {
             map.panTo(marker.getPosition());
             setIsVisible(prevState => !prevState);
           }}
-        />
+        >
+        </MapMarker>
         {isVisible &&
           <CustomOverlayMap position={{
-            lat: lat,
-            lng: lng
+            lat: content.LOT,
+            lng: content.LAT,
           }}>
-            {content}
+          <Maker onClick={()=>{setIsVisible(false)}}>
+            <img src={`${content.MAIN_IMG}`} className='img'></img>
+            <p>{`${content.DATE}`}</p>
+            <p>{`${content.PLACE}`}</p>
+            <div className='title'>{content.TITLE}</div>
+          </Maker>
           </CustomOverlayMap>
         }
       </>
     );
   });
+
+
 
   useEffect(() => {
     const token = localStorage.getItem('authToken');
@@ -121,9 +207,9 @@ const MapView = React.memo((props) => {
             minLevel={1}
             disableClickZoom={true}
           >
-            {flow.map((pos) => (
+            {flow.map((pos,idx) => (
               <MapMarker
-                key={`${pos.lat}-${pos.lng}`}
+                key={`${pos.lat}-${pos.lng}+${idx}`}
                 position={{
                   lat: pos.lat,
                   lng: pos.lng,
@@ -132,12 +218,15 @@ const MapView = React.memo((props) => {
             ))}
           </MarkerClusterer>
         ) : (
-            flow.map((value) => (
+            forumData.map((value,idx) => (
               <EventMarkerContainer
-                key={`EventMarkerContainer-${value.lat}-${value.lng}`}
-                lat={value.lat}
-                lng={value.lng}
-                content={value.content}
+               key={`EventMarkerContainer-${value.LAT}-${value.LOT}-${idx}`}
+                lat={parseFloat(value.LOT)}
+                lng={parseFloat(value.LAT)}
+                content={value}
+                
+                
+
               />
             ))
           )}
@@ -171,7 +260,9 @@ const MapView = React.memo((props) => {
 
       </Map>
     </ToSpot.Container>
+
   );
 });
 
 export default MapView;
+
